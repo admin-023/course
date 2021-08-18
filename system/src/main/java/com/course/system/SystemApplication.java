18959d269e5a391934df8996fa0a1a3edf1b346e
hujianhui <thishjh@foxmail.com>
2021-08-19 04:31:25 +0800

    210819 2nd 完成system注册到注册中心 第1步，增加eureka-client依赖 第2步，增加application.properties配置，指向注册中心的地址（eureka.client.service-url.defaultZone=http://localhost:8761/eureka/） 第3步，给启动类加上EnableEurekaClient注解

---
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -0,0 +1,22 @@
+package com.course.system;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SystemApplication {
+
+	private static final Logger LOG= LoggerFactory.getLogger(SystemApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(SystemApplication.class);
+		Environment env = app.run(args).getEnvironment();//getEnviroment得到与操作系统，处理器，屏幕等系统相关信息
+		LOG.info("启动成功");
+		LOG.info("System地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+	}
+}
