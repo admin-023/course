cd12fc31cf246983570814b7cd2917172325e229
Hugo <thishjh@foxmail.com>
2021-08-23 20:50:09 +0800

    测试2

---
+++ system/src/main/java/com/course/system/controller/TestController2.java
@@ -0,0 +1,19 @@
+package com.course.system.controller;
+
+import com.course.server.domain.Test;
+import com.course.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController /*@RestController返回json数据 @Controller返回页面*/
+public class TestController2 {
+   @Resource
+    private TestService testService;
+    @RequestMapping("/test")
+    public List<Test> test(){
+      return testService.list();
+  }
+}
