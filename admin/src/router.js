fbc0025a2d33938d63205a43ae4003a78c3d7438
Hugo <thishjh@foxmail.com>
2021-09-02 16:42:28 +0800

    增加了welcome子组件，增加了/admin/welcome子路由  router-view和子路由配合使用 注意：子路由的配置不要以“/”开头

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,9 +1,10 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Login from "./views/login.vue"
+import Admin from "./views/admin.vue"
+import Welcome from "./views/admin/welcome.vue"
 
 Vue.use(Router);
-
 export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
@@ -14,6 +15,13 @@ export default new Router({
     }, {
         path: '/login',
         component: Login
-    }
+    },{
+        path: '/admin',
+        component: Admin,
+            children:[{
+            path:'welcome',
+                component:Welcome,
+            }]
+        }
     ]
 })
