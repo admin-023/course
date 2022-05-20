bbb4cd3040a1ae6a1a83e6f1d27da261f23da1af
admin-023 <thishjh@foxmail.com>
2022-05-21 00:17:42 +0800

    课程管理和大章管理相互跳转

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -45,12 +45,15 @@
                             <span class="badge badge-info">{{course.time | formatSecond}}</span>
                         </p>
                         <p>
-                            <button v-on:click="edit(course)" class="btn btn-xs btn-info">
-                                             <i class="ace-icon fa fa-pencil bigger-120"></i>
-                                               </button>
-                                            <button v-on:click="del(course.id)" class="btn btn-xs btn-danger">
-                                                   <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                                             </button>
+                            <button v-on:click="toChapter(course)" class="btn btn-white btn-xs btn-info btn-round">
+                                大章
+                            </button>&nbsp;
+                            <button v-on:click="edit(course)" class="btn btn-white btn-xs btn-info btn-round">
+                                编辑
+                            </button>&nbsp;
+                            <button v-on:click="de l(course.id)" class="btn btn-white btn-xs btn-info btn-round">
+                                删除
+                            </button>
                         </p>
                     </div>
                 </div>
@@ -274,6 +277,14 @@
                         }
                     })
                 });
+            },
+            /**
+             * 点击【大章】
+             */
+            toChapter(course) {
+                let _this = this;
+                SessionStorage.set("course", course);
+                _this.$router.push("/business/chapter");
             }
         }
     }
